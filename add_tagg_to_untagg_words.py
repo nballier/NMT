146ddb2424f0b8e5eec4e96d41b86c118e1cc04b
lmerat46 <merat.laure@hotmail.fr>
2019-11-29 14:26:37 +0100

    update + readme

--- add_tagg_to_untagg_words.py
+++ add_tagg_to_untagg_words.py
@@ -11,15 +11,23 @@ list_words = []
 for lines in source.readlines():
 	list_words += lines.split(' ')
 
-print(list_words)
-
 for i in range(0, len(list_words)):
-        if "|" not in list_words[i] and list_words[i].find(".") == -1:
-                list_words[i] += "|none"
+	if "|" not in list_words[i] and list_words[i].find(".") == -1 and list_words[i].find("_")== -1:
+		list_words[i] += "|none"
+	if "_" in list_words[i] and ("|") in list_words[i]:
+		split = list_words[i].split("|")
+		group = split[1]
+		ss_list_words = split[0].split("_")
+		for j in range(0, len(ss_list_words)):
+			ss_list_words[j] = ss_list_words[j] + "|" + group
+		list_words[i] = " ".join(ss_list_words)
+	if "_" in list_words[i]:
+		ss_list_words = list_words[i].split("_")
+		for j in range(0, len(ss_list_words)):
+			ss_list_words[j] = ss_list_words[j] + "|mwe"
+		list_words[i] = " ".join(ss_list_words)
 source.close()
 
 source = open(args.source, 'w')
-for i in range(0, len(list_words)-1):
-	source.write(list_words[i] + ' ')
-source.write(list_words[-1])
+source.write(" ".join(list_words))
 source.close()
